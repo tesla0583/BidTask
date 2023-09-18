a7645d6df378bcf0965b58a797b17ed48f30234e
tesla0583 <firdavskhodjaev@gmail.com>
2023-09-18 15:20:36 +0500

    fix: removed comments

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BidController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -25,11 +15,3 @@ Route::prefix('requests')->controller(BidController::class)->group(function () {
     Route::put('/{id}', 'update');
 });
 
-//Route::controller(BidController::class)->group(function () {
-//    Route::post('/requests', 'store');
-//    Route::get('/requests', 'index');
-//});
-
-//Route::post('/requests', [BidController::class, 'store']);
-//Route::get('/requests', [BidController::class, 'index']);
-//Route::put('/{id}', [BidController::class, 'update']);
